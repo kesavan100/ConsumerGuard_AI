a3e1b123e1703fe1a8e45180b2025d1e0a1f9f25
Kesavan S <168976075+kesavan100@users.noreply.github.com>
2026-07-17 13:13:55 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,7 +5,14 @@
 # Azure App Service calls this script on startup.
 # The app must listen on port 8000 (Azure's default port for custom apps).
 
-pip install -r requirements.txt
+# Activate the virtual environment created by Azure Oryx build
+if [ -d "/home/site/wwwroot/antenv" ]; then
+    source /home/site/wwwroot/antenv/bin/activate
+elif [ -d "antenv" ]; then
+    source antenv/bin/activate
+elif [ -d "venv" ]; then
+    source venv/bin/activate
+fi
 
 streamlit run app.py \
     --server.port 8000 \
